7a7e50b7c7bdfd1806c3da3bc2471d126a6d7957
Sushil-19 <dubesushil19007@gmail.com>
2024-05-04 12:28:35 +0530

    created node js authentication project using ejs and passport library

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://dubesushil99:password123@cluster0.2eykogt.mongodb.net/?retryWrites=true'
+}
+
+//mongodb+srv://dubesushil99:<password>@cluster0.2eykogt.mongodb.net/
